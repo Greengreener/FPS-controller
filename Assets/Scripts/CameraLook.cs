562798607b6ce52dc44aae7fd6d17112310d3757
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-09-15 09:55:26 +1000

    1vs1Greybox

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -15,6 +15,9 @@ public class CameraLook : MonoBehaviour
     public float sensetivity = 100;
     public float minY = -60, maxY = 60;
     private float _rotY;
+
+    private Light torch;
+    bool torchOnOff;
     void Start()
     {
         //Lock the cursor
@@ -25,9 +28,18 @@ public class CameraLook : MonoBehaviour
         {
             axis = RotationalAxis.MouseY;
         }
+        torch = GetComponentInChildren<Light>();
+        torch.gameObject.SetActive(false);
+        torchOnOff = !torch.gameObject.activeSelf;
+
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            torch.gameObject.SetActive(torchOnOff);
+            torchOnOff = !torchOnOff;
+        }
         if (axis == RotationalAxis.MouseX)
         {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensetivity * Time.deltaTime, 0);
@@ -38,5 +50,6 @@ public class CameraLook : MonoBehaviour
             _rotY = Mathf.Clamp(_rotY, minY, maxY);
             transform.localEulerAngles = new Vector3(-_rotY, 0, 0);
         }
+        
     }
 }
